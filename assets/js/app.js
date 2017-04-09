98e22fcd7c3aade0cfb1117d4af7723f2eaacf43
mikemajesty <mikemajesty2011@gmail.com>
2017-04-09 11:35:35 -0300

    add tooltip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-angular.module('app', []);
+angular.module('app', ['720kb.tooltips']).config(['tooltipsConfProvider', function configConf(tooltipsConfProvider) {
+  tooltipsConfProvider.configure({
+    'smart':true,
+    'size':'large',
+    'speed': 'slow',
+    'tooltipTemplateUrlCache': true
+    //etc...
+  });
+}]);
