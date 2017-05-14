60702601d2f512e8a52fc44be8ad2722f2396b2b
mikemajesty <mikemajesty2011@gmail.com>
2017-05-14 15:34:59 -0300

    add personal project translate

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -19,14 +19,26 @@
 					$("#idiomas").text("IDIOMAS");
 					$("#portugues").html("Português <span class='lang-desc' id='nativo'>(Nativo)</span>");
 					$("#ingles").html("Inglês <span class='lang-desc'>(Intermediário)</span>");
-					$("#experience").html("<i id='exp' class='fa fa-briefcase'> </i> Experiências");
+					$("#experience").html("<i id='exp' class='fa fa-briefcase'></i>Experiências");
 					$("#jobtegra-title").text("Desenvolvedor Web");
 					$("#job-desc-tegra").text("Desenvolvimento em diversas plataformas e linguagens como: C#, Java , Node Js e Angular.");
 					$("#tegra-job-time").text("2015 - Atualmente");
 					$("#my-project").html("<i id='project' class='fa fa-archive'></i>Projetos");
+					//projetos
+					$("#persor-project").text("Projetos pessoais");
+					$("#chocobo").text("Componente de calendário para range de datas em AngularJs.");
+					$("#coovalidator").text("Biblioteca em c# para validação de TextBox e objetos.");
+					$("#excalibur").text("Componente de Dual List em AngularJs e bootstrap.");
+					$("#vuemonthcalendar").text("Componente para escolha de mês e ano em VueJs e VueMaterial.");
+					$("#coolreport").text("Biblioteca em c# para gerar relatórios com Entity Framework.");
+					$("#vueduallist").text("Componente de Dual List em VueJs e VueMaterial.");
+					$("#beaconmusic").text("Jukebox para pubs, integrado com um aplicativo mobile.");
+					$("#cooldatagrid").text("Biblioteca em c# para melhorar a interface de um DataGridView.");
+					$("#comandafacil").text("Sistema de comanda eletronica para comércios.");
+					$("#coolsync").text("Biblioteca em c# para execução em multithread.");
 				}
 				if (language === 'SP') {
-					$("#introduction").text(`Tengo ${$scope.year} años de experiencia en el desarrollo web en proyectos financieros y al por menor, con experiencia en múltiples plataformas como C #, Java, NodeJS y ángulo, obteniendo siempre papel en los proyectos que participaron. La licenciatura en sistemas de información, vivió con muchos desafíos en este periodo debido a mi proactividad y dinamismo, siempre sorprendiendo a los desafíos asignados.`);
+					$("#introduction").text(`Tengo ${$scope.year} años de experiencia en el desarrollo web en proyectos financieros y al por menor, con experiencia en múltiples plataformas como C#, Java, NodeJS y ángulo, obteniendo siempre papel en los proyectos que participaron. La licenciatura en sistemas de información, vivió con muchos desafíos en este periodo debido a mi proactividad y dinamismo, siempre sorprendiendo a los desafíos asignados.`);
 					$('.tagline').text('Desarrollador Full Stack');
 					$('#perfil').text("PERFIL PROFESIONAL");
 					$("#formacao").text("Formación");
@@ -35,14 +47,26 @@
 					$("#idiomas").text("IDIOMAS");
 					$("#portugues").html("Portugués <span class='lang-desc' id='nativo'>(Nativo)</span>");
 					$("#ingles").html("Inglés <span class='lang-desc'>(Intermedio)</span>");
-					$("#experience").html("<i id='exp' class='fa fa-briefcase'> </i> Experiencias");
+					$("#experience").html("<i id='exp' class='fa fa-briefcase'></i>Experiencias");
 					$("#jobtegra-title").text("Desarrollador Web");
-					$("#job-desc-tegra").text("Desarrollo en diversas plataformas y lenguajes como C #, Java, Node Js y Angular.");
+					$("#job-desc-tegra").text("Desarrollo en diversas plataformas y lenguajes como C#, Java, Node Js y Angular.");
 					$("#my-project").html("<i id='project' class='fa fa-archive'></i>Proyectos");
 					$("#tegra-job-time").text("2015 - Actualmente");
+					//projetos
+					$("#persor-project").text("Proyectos personales");
+					$("#chocobo").text("Componente de calendario para rango de fechas en AngularJs.");
+					$("#coovalidator").text("Biblioteca en C# para la validación de TextBox y objetos.");
+					$("#excalibur").text("Componente de Dual List en AngularJs y bootstrap.");
+					$("#vuemonthcalendar").text("Componente para elección de mes y año en VueJs y VueMaterial.");
+					$("#coolreport").text("Biblioteca en C# para generar informes con Entity Framework.");
+					$("#vueduallist").text("Componente de doble lista en VueJs y VueMaterial.");
+					$("#beaconmusic").text("Jukebox para pubs, integrado con una aplicación móvil.");
+					$("#cooldatagrid").text("Biblioteca en C# para mejorar la interfaz de un DataGridView.");
+					$("#comandafacil").text("Sistema de codigo de barras para comercios.");
+					$("#coolsync").text("Biblioteca en C# para ejecutarse en multiproceso.");
 				}
 				if (language === 'US' || language === 'UK') {
-					$("#introduction").text(`I have ${$scope.year} years of experience in web development in financial and retail projects, with experience in several platforms such as C #, Java, NodeJs and Angular, always taking center stage in the projects I participated in. Bachelor in information systems, I lived with several challenges in this period due to my proactivity and dynamism, always surprising in the challenges assigned.`);
+					$("#introduction").text(`I have ${$scope.year} years of experience in web development in financial and retail projects, with experience in several platforms such as C#, Java, NodeJs and Angular, always taking center stage in the projects I participated in. Bachelor in information systems, I lived with several challenges in this period due to my proactivity and dynamism, always surprising in the challenges assigned.`);
 					$('.tagline').text('Full Stack Developer');
 					$('#perfil').text("PROFESSIONAL PROFILE");
 					$("#formacao").text("Formation");
@@ -51,11 +75,23 @@
 					$("#idiomas").text("LANGUAGES");
 					$("#portugues").html("Portuguese <span class='lang-desc' id='nativo'>(Native)</span>");
 					$("#ingles").html("English <span class='lang-desc'>(Intermediate)</span>");
-					$("#experience").html("<i id='exp' class='fa fa-briefcase'> </i> Experience");
+					$("#experience").html("<i id='exp' class='fa fa-briefcase'></i>Experience");
 					$("#jobtegra-title").text("Web Developer");
-					$("#job-desc-tegra").text("Development in several platforms and languages like C #, Java, Node Js and Angular.");
+					$("#job-desc-tegra").text("Development in several platforms and languages like C#, Java, Node Js and Angular.");
 					$("#tegra-job-time").text("2015 - Currently");
-					$("#my-project").html("<i id='project' class='fa fa-archive'></i>Projects");
+					$("#my-project").html("<i id='project' class='fa fa-archive'></i>  Projects");
+					//projetos
+					$("#persor-project").text("Personal projects");
+					$("#chocobo").text("Calendar component for date range in AngularJs.");
+					$("#coovalidator").text("C# Library for validation of TextBox and objects.");
+					$("#excalibur").text("Dual List component in AngularJs and bootstrap.");
+					$("#vuemonthcalendar").text("Component for choosing month and year in VueJs and VueMaterial.");
+					$("#coolreport").text("C# Library to generate reports with Entity Framework.");
+					$("#vueduallist").text("Dual List component in VueJs and VueMaterial.");
+					$("#beaconmusic").text("Jukebox for pubs, integrated with a mobile application.");
+					$("#cooldatagrid").text("C# Library to improve the interface of a DataGridView.");
+					$("#comandafacil").text("Bar code system for trades.");
+					$("#coolsync").text("C# library for multithreaded execution.");
 				}
 			};
 
