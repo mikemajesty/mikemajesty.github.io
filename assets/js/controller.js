a199fc975c1fdc1717b0875b32680404fdd77ea6
mikemajesty <mikemajesty2011@gmail.com>
2018-04-08 22:42:07 -0300

    add more features

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -33,20 +33,20 @@
 					$("#p-pp").html("<i id='personal-projects' class='fa fa-child'></i>Projetos pessoais");
 					$("#br-partners").text("Novo Portal de Recursos Humanos do BR Partners que faz a gestão de todo o departamento. O sistema possibilitou em um acompanhamento excelente das atividades dos funcionário, com métricas e relatórios que agregaram muito para o banco.");
 					$("#assai").text("Sistema de conciliação de Notas fiscais, o sistema permite a comparação entre notas fiscais lançadas pelo Sefaz com as notas fiscais lançadas manualmente pelo departamento fiscal do Assaí, oferecendo uma rápida validação se existe discrepância entre os lançamentos.");
-					$("#s2m").text("Sistema desenvolvido diretamente para o Presidente do Santander Brasil, com a função de fornecer gráficos e métricas de todos os gestores de entrega do banco, fornecendo assim material para analise individual e geral das entregas dos projetos do Santander.");
+					$("#s2m").text("Sistema desenvolvido diretamente para o Presidente do Santander Brasil, com a função de fornecer gráficos e métricas de todos os gestores de entrega do banco, fornecendo assim material para análise individual e geral da saúde dos projetos do Santander.");
 					$("#prudential").text("Sistema para premiação de colaboradores, o sistema disponibiliza métricas e estatísticas de cada colaborador, permitindo assim saber dentre eles qual obteve o maior destaque, por esses motivos o sistema obteve um grande destaque entre os gestores.");
 					$("#githubapi").text("API para gerar dados estatísticos a partir do github do usuário.");
 					$("#horus").text("ERP que controla todas as informações da empresa. O sistema está atualmente em todas as filias do Assaí, isso abrange mais de 120 lojas, o sistema gerencia dados, recursos e processos, aumentando significamente o poder do Assaí em tomada de decisões.");
 					//projetos
 					$("#persor-project").text("Projetos pessoais");
-					$("#chocobo").text("Componente de calendário para range de datas em AngularJs.");
+					$("#chocobo").text("Calendário em AngularJS para escolher um intervalo de datas.");
 					$("#coovalidator").text("Biblioteca em C# para validação de TextBox e objetos.");
-					$("#excalibur").text("Componente de Dual List em AngularJs e bootstrap.");
+					$("#excalibur").text("Dual List em AngularJs e bootstrap.");
 					$("#vuemonthcalendar").text("Componente para escolha de mês e ano em VueJs e VueMaterial.");
 					$("#stormreport").text("Biblioteca em C# para facilitar na criação de relatório em ASP.NET.");
-					$("#vueduallist").text("Componente de Dual List em VueJs e VueMaterial.");
+					$("#vueduallist").text("Dual List em VueJs e VueMaterial.");
 					$("#beaconmusic").text("Jukebox para pubs, integrado com um aplicativo mobile.");
-					$("#cooldatagrid").text("Biblioteca em C# para melhorar a interface de um DataGridView.");
+					$("#cooldatagrid").text("Biblioteca em C# para melhorar a interface gráfica de uma TableView");
 					$("#legendofgithub").text("Jogo baseado nas estatísticas do github do usuário.");
 
 					//projetos hackaton
@@ -60,6 +60,9 @@
 					$("#payforce").text("(SANTANDER 2016 - Semifinalista) - Carteira digital que usa seu número de celular como token de pagamento.");
 					$("#relato-serasa").text("(SERASA EXPERIAN 2017) - Assistente digital para auxiliar a análise de crédito.");
 					$("#skill-title").html("<i id='skill' class='fa fa-rocket'></i>Habilidades &amp; Proficiências");
+
+					$("#nuget-lib").text("6 Bibliotecas");
+					$("#npm-lib").text("4 Bibliotecas");
 				}
 				if (language === 'SP') {
 					$("#introduction").text(`Tengo ${$scope.year} años de experiencia en el desarrollo web en proyectos financieros y al por menor, con experiencia en múltiples plataformas como C#, Java, NodeJS y ángulo, obteniendo siempre papel en los proyectos que participaron. La licenciatura en sistemas de información, vivió con muchos desafíos en este periodo debido a mi proactividad y dinamismo, siempre sorprendiendo a los desafíos asignados.`);
@@ -85,19 +88,20 @@
 					$("#ibm-job-time").text("Jan/2018 - Actualmente");
 					$("#br-partners").text("Nuevo Portal de Recursos Humanos de BR Partners que hace la gestión de todo el departamento. El sistema posibilitó en un acompañamiento excelente de las actividades de los empleados, con métricas e informes que agregaron mucho para el banco.");
 					$("#assai").text("El sistema permite la comparación entre notas fiscales lanzadas por el Sefaz con las notas fiscales lanzadas manualmente por el departamento fiscal del Assaí, ofreciendo una rápida validación si existe discrepancia entre los lanzamientos.");
-					$("#s2m").text("Sistema desarrollado directamente para el Presidente de Santander Brasil, con la función de proporcionar gráficos y métricas de todos los gestores de entrega del banco, proporcionando así material para el análisis individual y general de las entregas de los proyectos del Santander.");
+					$("#s2m").text("Sistema desarrollado directamente para el Presidente del Santander Brasil, con la función de proporcionar gráficos y métricas de todos los gestores de entrega del banco, proporcionando así material para análisis individual y general de la salud de los proyectos del Santander.");
 					$("#prudential").text("El sistema de premiación de colaboradores, el sistema proporciona métricas y estadísticas de cada colaborador, permitiendo así saber entre ellos cuál obtuvo el mayor destaque, por esos motivos el sistema obtuvo un gran destaque entre los gestores.");
 					$("#githubapi").text("API para generar datos estadísticos desde el github del usuario.");
 					$("#horus").text("ERP que controla toda la información de la empresa. El sistema está actualmente en todas las filas de Assaí, que abarca más de 120 tiendas, el sistema administra datos, recursos y procesos, aumentando significativamente el poder del Assaí en la toma de decisiones.");
 					//projetos
 					$("#persor-project").text("Proyectos personales");
-					$("#chocobo").text("Componente de calendario para rango de fechas en AngularJs.");
+					$("#chocobo").text("Calendario en AngularJS para elegir un intervalo de fechas.");
 					$("#coovalidator").text("Biblioteca en C# para la validación de TextBox y objetos.");
-					$("#excalibur").text("Componente de Dual List en AngularJs y bootstrap.");
+					$("#excalibur").text("Dual List en AngularJs y bootstrap.");
 					$("#vuemonthcalendar").text("Componente para elección de mes y año en VueJs y VueMaterial.");
 					$("#stormreport").text("Biblioteca en C# para facilitar la creación de informes en ASP.NET.");
-					$("#vueduallist").text("Componente de doble lista en VueJs y VueMaterial.");
+					$("#vueduallist").text("Dual List en VueJs y VueMaterial.");
 					$("#beaconmusic").text("Jukebox para pubs, integrado con una aplicación móvil.");
+					$("#cooldatagrid").text("Biblioteca en C # para mejorar la interfaz gráfica de una tabla.");
 					$("#legendofgithub").text("Juego basado en las estadísticas del github del usuario.");
 					//projetos hackaton
 
@@ -111,6 +115,9 @@
 					$("#payforce").text("(SANTANDER 2016 - Semifinalista) - Cartera digital que utiliza su número de móvil como token de pago.");
 					$("#relato-serasa").text("(SERASA EXPERIAN 2017) - Asistente digital para auxiliar el análisis de crédito.");
 					$("#skill-title").html("<i id='skill' class='fa fa-rocket'></i>HABILIDADES Y PROFICIENCIAS");
+
+					$("#nuget-lib").text("6 Bibliotecas");
+					$("#npm-lib").text("4 Bibliotecas");
 				}
 				if (language === 'US' || language === 'UK') {
 					$("#introduction").text(`I have ${$scope.year} years of experience in web development in financial and retail projects, with experience in several platforms such as C#, Java, NodeJs and Angular, always taking center stage in the projects I participated in. Bachelor in information systems, I lived with several challenges in this period due to my proactivity and dynamism, always surprising in the challenges assigned.`);
@@ -136,20 +143,21 @@
 					$("#p-pp").html("<i id='personal-projects' class='fa fa-child'></i>Personal projects");
 					$("#br-partners").text("BR Partners New Human Resources Portal that manages the entire department. The system enabled excellent monitoring of employees' activities, with metrics and reports that added a lot to the bank.");
 					$("#assai").text("System of reconciliation of invoices, the system allows the comparison between invoices issued by Sefaz with the invoices manually released by the fiscal department of Assaí, offering a quick validation if there is discrepancy between the launches.");
-					$("#s2m").text("System developed directly for the President of Santander Brasil, with the function of providing graphs and metrics of all the bank's delivery managers, thus providing material for individual and general analysis of deliveries of Santander projects.");
+					$("#s2m").text("System developed directly for the President of Santander Brasil, with the function of providing graphs and metrics of all the bank's delivery managers, thus providing material for individual and general health analysis of Santander's projects.");
 					$("#prudential").text("System for awarding employees, the system provides metrics and statistics of each employee, thus allowing them to know which one got the most prominence, for these reasons the system got a great prominence among the managers.");
 					$("#horus").text("ERP that controls all company information. The system is currently in all branches of Assaí, this covers more than 120 stores, the system manages data, resources and processes, significantly increasing Assaí's power in decision making.");
 					//projetos
 					$("#persor-project").text("Personal projects");
-					$("#chocobo").text("Calendar component for date range in AngularJs.");
+					$("#chocobo").text("Calendar in AngularJS to choose a date range.");
 					$("#coovalidator").text("Library in C # for TextBox and validation objects.");
-					$("#excalibur").text("Dual List component in AngularJs and bootstrap.");
+					$("#excalibur").text("Dual List in AngularJs and bootstrap.");
 					$("#vuemonthcalendar").text("Component for choosing month and year in VueJs and VueMaterial.");
 					$("#stormreport").text("Library in C# on ASP.NET to facilitate in report creation.");
-					$("#vueduallist").text("Dual List component in VueJs and VueMaterial.");
+					$("#vueduallist").text("Dual List in VueJs and VueMaterial.");
 					$("#beaconmusic").text("Jukebox for pubs, integrated with a mobile application.");
-					$("#cooldatagrid").text("Library in C # to improve the interface of a DataGridView.");
+					$("#cooldatagrid").text("C # library to improve the graphical interface of a Table.");
 					$("#githubapi").text("API to generate statistical data from the user's github.");
+					$("#legendofgithub").text("Game based on user github statistics.");
 					//projetos hackaton
 					$("#campeao").text("Champion");
 					$("#campeao2").text("Champion");
@@ -161,6 +169,9 @@
 					$("#payforce").text("(SANTANDER 2016 - Semifinalist) - Digital wallet that uses your mobile number as payment token.");
 					$("#relato-serasa").text("(SERASA EXPERIAN 2017) - Digital assistant to assist in credit analysis.");
 					$("#skill-title").html("<i id='skill' class='fa fa-rocket'></i>SKILLS & PROFICIENCIES");
+
+					$("#nuget-lib").text("6 Libraries");
+					$("#npm-lib").text("4 Libraries");
 				}
 			};
 
